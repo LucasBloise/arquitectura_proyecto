5f70b50299a2d8ce35a9c76b5e10ab2a5f8dc4b7
Agustín <adevoto@clarika.com.ar>
2022-10-13 11:44:10 -0300

    printTable

--- src/main/java/com/mycompany/proyecto/arq/Controllers/GraficoController.java
+++ src/main/java/com/mycompany/proyecto/arq/Controllers/GraficoController.java
@@ -6,13 +6,34 @@ public class GraficoController {
     public static void imprimirTabla() {
         String posicion;
         for (int i = 0; i < 7; i++) {
-            for (int j = 0; j < 1000; j++) {
+
+            for (int j = 0; j < 35; j++) {
                 posicion = grafico[i][j];
+                if (i==0&&j==0) System.out.print("| Listo     |");
+                if (i==1&&j==0) System.out.print("| Bloqueado |");
+                if (i==2&&j==0) System.out.print("| P1        |");
+                if (i==3&&j==0) System.out.print("| P2        |");
+                if (i==4&&j==0) System.out.print("| P3        |");
+                if (i==5&&j==0) System.out.print("| SO        |");
+                if (i==6&&j==0) System.out.print("| Tiempo    |");
+                
                 if (posicion == null) {
                     posicion = "   ";
                 }
-                System.out.print(posicion);
+                if (i==6) {
+                    posicion = ""+j;
+                    System.out.print("");
+                }
+                while (posicion.length() < 3) {
+                    posicion += " ";
+                }
+                System.out.print(posicion + " |");
+
             }
+            System.out.println();
+            
+            System.out.print("-------------------------------------------------------------------------------------------------------------------");
+            
             System.out.println();
         }
     }
