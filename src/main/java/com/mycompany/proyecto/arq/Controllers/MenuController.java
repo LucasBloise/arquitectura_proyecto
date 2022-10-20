c7dfbd25216497eac26dcc4f835ed14fa2835711
Lucas Bloise <lucas.bloise@decemberlabs.com>
2022-10-20 17:14:46 -0300

    carga automatica procesos

--- src/main/java/com/mycompany/proyecto/arq/Controllers/MenuController.java
+++ src/main/java/com/mycompany/proyecto/arq/Controllers/MenuController.java
@@ -22,6 +22,7 @@ public class MenuController {
         System.out.println("3 - Mostrar grafica de JSF");
         System.out.println("4 - Mostrar grafica de JSF/D");
         System.out.println("5 - Limpiar cola de Procesos");
+        System.out.println("6 - Carga automatica de Procesos");
         System.out.println("Pulse enter para salir");
         System.out.println("-----------------------------------");
     }
@@ -77,6 +78,11 @@ public class MenuController {
                 System.out.println("Procesos eliminados");
                 System.out.println("Presiona enter para volver al menu");
                 sc.nextLine();
+                break;
+            case "6":
+                System.out.print("\033[H\033[2J");
+                System.out.println("Va a cargar procesos automaticamente.");
+                ProcesoController.cargaAutomatica();
                 break;
             default:
                 System.exit(0);
